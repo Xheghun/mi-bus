b2d814fe127d89594cf068471e4d1a6ba24f52c0
Xheghun <ysenruf@gmail.com>
2021-04-20 23:31:22 +0400

    inro screen

---
+++ lib/app/intro/intro_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  static const routeName = "/introScreen";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

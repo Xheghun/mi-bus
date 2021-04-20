85805b3b251d1b55b95fd55d5e981ef9058799e3
Xheghun <ysenruf@gmail.com>
2021-04-20 22:05:28 +0400

    basic theme setup

--- lib/util/styles/colors.dart
+++ lib/util/styles/colors.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
 const colorPrimary = Color(0xffC20E1A);
-const colorSecondary = Color(0xff000000);
+Color colorSecondary = Colors.black;
--- lib/util/styles/theme.dart
+++ lib/util/styles/theme.dart
@@ -1,1 +1,16 @@
-class AppTheme {}
+import 'package:flutter/material.dart';
+import 'package:myriad_bus_scheduler/util/styles/colors.dart';
+
+class AppTheme {
+  ThemeData main() {
+    return ThemeData(
+      primaryColor: colorPrimary,
+      accentColor: colorSecondary,
+      scaffoldBackgroundColor: Colors.white,
+      iconTheme: IconThemeData(
+        color: Colors.white,
+        size: 20,
+      ),
+    );
+  }
+}
